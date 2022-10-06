69da9bfde884fc4b0a886b456373863f4cd5e4a0
SogekingDaBest <sogekingdabest@gmail.com>
2022-10-06 19:11:27 +0200

    Implementación para que recoja mas noticias
    Básicamente se ha hecho que siga el botón de siguientes noticias.

--- projectV/projectV/spiders/vandal_spider.py
+++ projectV/projectV/spiders/vandal_spider.py
@@ -1,8 +1,8 @@
 import scrapy
 import json
-
 class VandalSpider(scrapy.Spider):
     name = "vandal"
+    num = 0
 
     def start_requests(self):
         urls = [
@@ -12,13 +12,22 @@ class VandalSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        next_page = response.xpath('//div[contains(@class,"caja620")]/a/@href').getall()
-        print("debugElement nextPage: ", next_page)
-        yield from response.follow_all(next_page, self.parseArticle)
+        news = response.xpath('//div[contains(@class,"caja620")]/a/@href').getall()
+        print("debugElement news: ", news)
+        filename = f' {self.num} vandal.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+        next_page = response.xpath('//div[contains(@class,"tn14b")]/a/@href').getall()
+        for i in range(0, len(next_page)):
+            next_page[i] = 'https://vandal.elespanol.com'+next_page[i]
+        print("soy nextpage: ", next_page)
+        yield from response.follow_all(next_page, callback=self.parse)
+        yield from response.follow_all(news, self.parseArticle)
 
     def parseArticle(self, response):
         jsonText = response.xpath('//div[contains(@id,"globalwrap")]/script/text()').get()
-        print("debugElementArticle ", jsonText)
+        # print("debugElementArticle ", jsonText)
         jsonData = json.loads(jsonText)
         
         yield {
