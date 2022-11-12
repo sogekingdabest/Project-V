4909832ac70d3bf4b220ac51e1c7a71f172764d0
SogekingDaBest <sogekingdabest@gmail.com>
2022-11-12 19:28:14 +0100

    Web funcional
    Creación de la web.

--- projectV/projectV/web/web.py
+++ projectV/projectV/web/web.py
@@ -3,41 +3,51 @@ from flask import Flask, flash, render_template, request, redirect
 from elasticsearch import Elasticsearch
 es = Elasticsearch('http://localhost:9200')
 
+DEFAULT_SIZE = 25
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    #cargar el "feed" (hacer una query ultimas noticias)
     
     if request.method == 'POST':
         return results()
-    
+
     return render_template('index.html', results="Pagina Principal")
 
 
 
 @app.route('/results', methods=['GET','POST'])
 def results():
-    
-    #search_string = request.form["search-input"]
-    print(request.form)
 
     search_input = request.form['search-input']
     console      = request.form['input-console']
     videogame    = request.form['input-videogame']
     company      = request.form['input-company']
-    date         = request.form['input-date']
+    date_ini     = request.form['input-date-ini']
+    date_end     = request.form['input-date-end']
     author       = request.form['input-author']
 
-
-    #https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
-
+    size = DEFAULT_SIZE
+    
+    filters = []
+    if console != "ANY":
+        filters.append({"term" : {"consoles": console}})
+    if videogame != "ANY":
+        filters.append({"term" : {"games": videogame}})
+    if company != "ANY":
+        filters.append({"term" : {"companies": company}})
+    if author != "":
+        filters.append({"term" : {"author": author}})
+    if date_ini != "":
+        filters.append({"range" : {"date": {"gte": date_ini}}})
+    if date_end != "":
+        filters.append({"range" : {"date": {"lte": date_end}}})
 
     #peticiones a elastic
     body_query = {
+        "size" : size,
         "query" : {
             "bool": {
                 "must": {
@@ -46,22 +56,13 @@ def results():
                         "fields" : ["title", "description","articleBody"]
                     }
                 },
-                "filter":{
-                    "consoles": console,
-                    "companies": company,
-                    "games": videogame,
-                    "author": author,
-                    "date": date
-                }
+                "filter": filters                
             }
         }
     }
 
-    articulos = es.search(index="articulos", query=body_query)["hits"]["hits"]
-
-    for articulo in articulos:
-        print(articulo)
-
+    articulos = es.search(index="articulos", body=body_query)["hits"]["hits"]
+    print(len(articulos))
 
 
     return render_template('index.html',  noticias=articulos)
